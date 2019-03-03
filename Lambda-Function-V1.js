219649c00c293c23d7198b47250d3899fb373e22
viveknigam3003 <viveknigam.nigam3@gmail.com>
2019-03-03 21:27:13 +0530

    New Updates

---
+++ Lambda-Function-V1.js
@@ -0,0 +1,150 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+const cookbook = require('./alexa-cookbook.js');
+
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
+
+const SKILL_NAME = 'Psychology Facts';
+const GET_FACT_MESSAGE = 'Here\'s your fact: ';
+const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const FALLBACK_MESSAGE = 'The Psychology Facts skill can\'t help you with that.  It can help you discover facts about space if you say tell me a space fact. What can I help you with?';
+const FALLBACK_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+//=========================================================================================================================================
+//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-nodejs-fact/tree/en-US/lambda/data
+//=========================================================================================================================================
+
+const data = [
+  'Any friendship that was born in the period between 16 and 28 years of age is more likely to be robust and long lasting.',
+  'Women generally prefer men with deep husky voices because they seem more confident and not aggressive.',
+  'The people who give the best advice are usually the ones with the most problems.',
+  'The smarter the person is, the faster he thinks, and the sloppier his handwriting is.',
+  'Our emotions don’t affect the way we communicate. In fact, the very opposite is true: the way we communicate has an influence on our mood.',,
+  'The way a person treats restaurant staff reveals a lot about their character.',
+  'People who have a strong sense of guilt are better at understanding other people’s thoughts and feelings.',
+  'Men are not funnier than women: they just make more jokes, not caring whether other people like their humor or not.',
+  'Listening to high-frequency music makes you feel calm, relaxed, and happy.',
+  'Women have twice as many pain receptors on their bodies than men, but they have a much higher pain tolerance.',
+  'Doing things that scare you will make you happier.',
+  'The average amount of time a woman can keep a secret is 47 hours and 15 minutes.',
+  'People who try to keep everyone happy often end up feeling the loneliest.',
+  'When you hold the hand of a loved one, you feel pain less keenly and worry less.',
+  'Women who have mostly male friends stay in a good mood more often.',
+  'People who speak two languages may unconsciously shift their personalities when they switch from one language to another.',
+  'Being alone for a long time is as bad for your health as smoking 15 cigarettes a day',
+  'Travel boosts brain health and also decreases a person’s risk of heart attack and depression.',
+  'Marrying your best friend eliminates the risk of divorce by over 70%, and this marriage is more likely to last a lifetime.',
+  'People look more attractive when they speak about the things they are really interested in.',
+];
+
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.
+//=========================================================================================================================================
+
+const GetNewFactHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'LaunchRequest'
+      || (request.type === 'IntentRequest'
+        && request.intent.name === 'GetNewFactIntent');
+  },
+  handle(handlerInput) {
+    const randomFact = cookbook.getRandomItem(data);
+    const speechOutput = GET_FACT_MESSAGE + randomFact;
+
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .withSimpleCard(SKILL_NAME, randomFact)
+      .getResponse();
+  },
+};
+
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(HELP_MESSAGE)
+      .reprompt(HELP_REPROMPT)
+      .getResponse();
+  },
+};
+
+const FallbackHandler = {
+  // 2018-May-01: AMAZON.FallbackIntent is only currently available in en-US locale.
+  //              This handler will not be triggered except in that locale, so it can be
+  //              safely deployed for any locale.
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.FallbackIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(FALLBACK_MESSAGE)
+      .reprompt(FALLBACK_REPROMPT)
+      .getResponse();
+  },
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(STOP_MESSAGE)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, an error occurred.')
+      .reprompt('Sorry, an error occurred.')
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    GetNewFactHandler,
+    HelpHandler,
+    ExitHandler,
+    FallbackHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
